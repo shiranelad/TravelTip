e069b6340da90831b30d9472bfcaf5bb8a4d45eb
Shiran Elad <shiranelad@gmail.com>
2022-02-23 23:37:57 +0200

    final commit

--- js/app.controller.js
+++ js/app.controller.js
@@ -1,8 +1,6 @@
 import { locService } from './services/loc.service.js'
 import { mapService } from './services/map.service.js'
 
-
-
 window.onload = onInit;
 window.onPanTo = onPanTo;
 window.onGetLocs = onGetLocs;
